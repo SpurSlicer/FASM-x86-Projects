99950738f6b37c8b69daae9cd29b6c0fc80ef24d
SpurSlicer <104993935+SpurSlicer@users.noreply.github.com>
2022-07-31 20:30:48 -0600

    Made the prompts make sense

--- Interaction.asm
+++ Interaction.asm
@@ -5,12 +5,12 @@ include 'macro/import32.inc'                    ;imports multipurpose macro asse
 
 section 'data' data readable writeable
         msg1 db 'Hello!',10,0
-        msg2 db 'Want to test cmp [y/n]? ',0
+        msg2 db 'Want to test cmp [y/x]? ',0
         n db 'n',0
         resp db '' ,0
         formatin db '%c',0
         yresp db 'you said y!',10,0
-        nresp db 'you said n!',10,0
+        nresp db 'you said something other than y!',10,0
         exiting db 'exiting...',10,0
         p db 'pause>nul',10,0                   ;this pauses until a key is pressed in the batch
 
@@ -18,7 +18,7 @@ section '.code' code readable executable        ;shows where the code starts
         main:                                   ;main pointer
                 push ebp                        ;sets up the stack
                 mov ebp, esp                    ;sets up the stack
-                sub ebp, 4
+                sub ebp, 1
                 mov dword [esp], msg1
                 call [printf]
                 mov dword [esp], p
@@ -53,5 +53,5 @@ section '.idata' import data readable           ;declares imported data section 
         import msvcrt,\                         ;imports said library
         printf, 'printf',\                      ;declares external function printf
         system, 'system',\                      ;declares external function system
-        scanf, 'scanf',\
+        scanf, 'scanf',\                        ;declares external function scanf
         exit,'exit'                             ;declares external function exit
